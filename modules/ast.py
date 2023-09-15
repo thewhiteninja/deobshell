ebd5f9dae86e016958a8e67d35ed2450b91167d2
Hendrik Eckardt <hendrik.eckardt@gdata-adan.de>
2023-09-15 18:08:13 +0200

    Add option to run PowerShell under docker

--- modules/ast.py
+++ modules/ast.py
@@ -20,16 +20,24 @@ def read_ast_file(filename):
         return None
 
 
-def create_ast_file(ps1_file):
+def create_ast_file(ps1_file, use_docker):
     log_info(f"Creating AST for: {ps1_file}")
 
-    cmd = ["PowerShell", "-ExecutionPolicy", "Unrestricted", "-File",
-           os.path.abspath(os.path.join("tools", "Get-AST.ps1")),
-           "-ps1", os.path.abspath(ps1_file)]
+    if use_docker:
+        cmd = ["docker", "run", "-v", os.path.abspath(os.path.join("tools", "Get-AST.ps1")) + ":/Get-AST.ps1",
+            "-v", os.path.abspath(ps1_file / "..") + ":/scriptdir",
+            "--net", "none", "--rm", "-it", "mcr.microsoft.com/powershell:lts-7.2-ubuntu-22.04",
+            "pwsh", "-File", "/Get-AST.ps1", "-ps1", "/scriptdir/" + ps1_file.name]
+    else:
+        cmd = ["PowerShell", "-ExecutionPolicy", "Unrestricted", "-File",
+            os.path.abspath(os.path.join("tools", "Get-AST.ps1")),
+            "-ps1", os.path.abspath(ps1_file)]
 
     result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
 
     for line in result.stdout.splitlines():
+        log_debug(line)
+    for line in result.stderr.splitlines():
         log_debug(line)
 
     return result.returncode == 0
