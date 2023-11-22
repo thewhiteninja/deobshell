thewhiteninja/deobshell

commit ebd5f9dae86e016958a8e67d35ed2450b91167d2

Add option to run PowerShell under docker

modules/ast.py
@@ -20,16 +20,24 @@ def read_ast_file(filename):
         return None
 
 
-def create_ast_file(ps1_file):
+def create_ast_file(ps1_file, use_docker):
     log_info(f"Creating AST for: {ps1_file}")
 
-    cmd = ["PowerShell", "-ExecutionPolicy", "Unrestricted", "-File",
-           os.path.abspath(os.path.join("tools", "Get-AST.ps1")),
-           "-ps1", os.path.abspath(ps1_file)]
+    if use_docker:
+        cmd = ["docker", "run", "-v", os.path.abspath(os.path.join("tools", "Get-AST.ps1")) + ":/Get-AST.ps1",
+            "-v", os.path.abspath(ps1_file / "..") + ":/scriptdir",
+            "--net", "none", "--rm", "-it", "mcr.microsoft.com/powershell:lts-7.2-ubuntu-22.04",
+            "pwsh", "-File", "/Get-AST.ps1", "-ps1", "/scriptdir/" + ps1_file.name]
+    else:
+        cmd = ["PowerShell", "-ExecutionPolicy", "Unrestricted", "-File",
+            os.path.abspath(os.path.join("tools", "Get-AST.ps1")),
+            "-ps1", os.path.abspath(ps1_file)]
 
     result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
 
     for line in result.stdout.splitlines():
+        log_debug(line)
+    for line in result.stderr.splitlines():
         log_debug(line)
 
     return result.returncode == 0

main.py
@@ -22,6 +22,8 @@ def usage():
     print("Options:")
     print("      -h, --help      : Show help")
     print("      -i, --in        : Input .ps1 file")
+    if sys.platform == "linux":
+        print("      --docker        : Run PowerShell under Docker")
     print()
     sys.exit(0)
 
@@ -35,6 +37,8 @@ def parse_args():
         elif sys.argv[i] in ["-i", "--in"]:
             OPTIONS["input"] = sys.argv[i + 1]
             i += 1
+        elif sys.argv[i] == "--docker":
+            OPTIONS["docker"] = True
         else:
             OPTIONS["command"] = sys.argv[i]
         i += 1
@@ -43,7 +47,7 @@ def parse_args():
 def deob(ps1_file):
     p = pathlib.Path(ps1_file)
 
-    if create_ast_file(p):
+    if create_ast_file(p, "docker" in OPTIONS):
 
         if ast := read_ast_file(p.with_suffix(".xml")):
             o = Optimizer()
@@ -59,7 +63,7 @@ def deob(ps1_file):
 def format(ps1_file):
     p = pathlib.Path(ps1_file)
 
-    if create_ast_file(p):
+    if create_ast_file(p, "docker" in OPTIONS):
 
         if ast := read_ast_file(p.with_suffix(".xml")):
             r = Rebuilder(p.with_suffix(".formatted.ps1"))

commit 349e8bb0e2ed634b7371bef9a70c381601b1869b

Fix invoke_member regression introduced by commit ebd5f9d

modules/ast.py
@@ -21,6 +21,12 @@ def read_ast_file(filename):
 
 
 def create_ast_file(ps1_file, use_docker):
+    global _g_use_docker  # Hack for optimizations that need to parse a sub-AST
+    if use_docker is None:
+        use_docker = _g_use_docker
+    else:
+        _g_use_docker = use_docker
+
     log_info(f"Creating AST for: {ps1_file}")
 
     if use_docker:

modules/optimizations/invoke_member.py
@@ -26,7 +26,7 @@ def opt_invoke_expression(ast):
                         with open(p, "w") as tmp:
                             tmp.write(script_content)
 
-                        if create_ast_file(p):
+                        if create_ast_file(p, None):
                             if sub_ast := read_ast_file(p.with_suffix(".xml")):
                                 log_debug("Replace Invoke-Expression by expression AST")
 