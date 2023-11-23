f07ef96071e4f009f7c0f25a0ce6696f86915fac
Hendrik Eckardt <hendrik.eckardt@gdata-adan.de>
2023-11-23 11:52:29 +0100

    opt_remove_uninitialised_variable_usage: Consider variable uninitialised if assigned from another uninitialised variable; bugfix for operators other than Plus

--- modules/optimizations/dead_codes.py
+++ modules/optimizations/dead_codes.py
@@ -1,6 +1,7 @@
 # coding=utf-8
-from modules.logger import log_debug
-from modules.utils import parent_map, replace_node, is_prefixed_var, get_used_vars
+from modules.logger import log_debug, log_err
+from modules.utils import create_constant_number, parent_map, replace_node, \
+    is_prefixed_var, get_used_vars, to_numeric
 
 
 def opt_unused_variable(ast):
@@ -26,14 +27,43 @@ def opt_unused_variable(ast):
 
 def opt_remove_uninitialised_variable_usage(ast):
     assigned = set()
+    kill_list = []
+    replacements = []
+
+    """
+    FIXME This function causes invalid output for constructs like
+    $i = 2;
+    while ($i -gt 0) {
+      $unassgn = $unassn + 4;
+      $i--;
+    }
+    Write-Host $unassgn
+    """
 
     for node in ast.iter():
         if node.tag in ["AssignmentStatementAst"]:
             subnodes = list(node)
+            if subnodes[1].tag == "CommandExpressionAst" and subnodes[1][0].tag == "VariableExpressionAst":
+                var_source = subnodes[1][0].attrib["VariablePath"].lower()
+                if var_source not in assigned:
+                    if not is_prefixed_var(var_source) and var_source != "_":
+                        var_dest = subnodes[0].attrib["VariablePath"].lower()
+                        log_debug(f"Remove variable '{var_dest}' assigned from unassigned variable '{var_source}'")
+                        kill_list.append(node)
+                        continue
+
             if subnodes[0].tag == "VariableExpressionAst":
                 assigned.add(subnodes[0].attrib["VariablePath"].lower())
-        if node.tag in ["BinaryExpressionAst"]:
+
+        elif node.tag in ["ParameterAst"]:
+            var_expr = node.find("VariableExpressionAst")
+            if var_expr is not None:
+                assigned.add(var_expr.attrib["VariablePath"].lower())
+
+        elif node.tag in ["BinaryExpressionAst"]:
             subnodes = list(node)
+
+            operator = node.attrib["Operator"]
 
             if subnodes[0].tag == "VariableExpressionAst":
                 variable = subnodes[0]
@@ -47,9 +77,22 @@ def opt_remove_uninitialised_variable_usage(ast):
             if variable is not None and other is not None:
                 if variable.attrib["VariablePath"].lower() not in assigned:
                     if not is_prefixed_var(variable.attrib["VariablePath"]):
+                        if operator == "Plus":
+                            replacements.append((node, other))
+                        elif operator in ["Minus", "Multiply"] and other.tag == "ConstantExpressionAst":
+                            replacements.append((variable, create_constant_number(0)))
+                        else:
+                            continue
+
                         log_debug("Remove unassigned variable use '%s'" % (variable.attrib["VariablePath"]))
-                        replace_node(ast, node, other)
 
-                        return True
+    if kill_list or replacements:
+        parents = parent_map(ast)
 
-    return False
+    for node in kill_list:
+        parents[node].remove(node)
+
+    for node, repl in replacements:
+        replace_node(ast, node, repl, parents=parents)
+
+    return len(kill_list) + len(replacements) != 0
