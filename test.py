276fe7f28399e4712e3b009020ed27825a8aa8d4
Nitecoder <nitecoder@gmail.com>
2024-01-03 20:22:34 -0800

    fix: make tests runnable again

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import os
 import shutil
 from unittest import TestCase
 
-from main import deob
+from main import deob, OPTIONS
 from modules.logger import set_log_level, LogLevel
 from modules.utils import make_directory, delete_directory
 
@@ -17,13 +17,17 @@ def diff_text(a, b):
     return list(difflib.unified_diff(text1, text2))
 
 
-def test(name):
+def test(folder, name):
+    src = os.path.join("data", folder, f"{name}.ps1.donotrun")
+    expected = os.path.join("data", folder, f"{name}.deob.ps1.donotrun")
     tmp_input = os.path.join("tmp", f"{name}.ps1")
+    tmp_output = os.path.join("tmp", f"{name}.deob.ps1")
 
-    shutil.copy2(os.path.join("data", f"{name}.ps1"), tmp_input)
-    deob(tmp_input)
+    shutil.copy2(src, tmp_input)
+    if not deob(tmp_input):
+        return -1
 
-    diff = diff_text(os.path.join("tmp", f"{name}.deob.ps1"), os.path.join("data", f"{name}.deob.ps1"))
+    diff = diff_text(expected, tmp_output)
     for line in diff:
         print(line)
 
@@ -34,17 +38,26 @@ class Test(TestCase):
     @classmethod
     def setUpClass(cls) -> None:
         make_directory("tmp")
+        # set_log_level(LogLevel.DEBUG, 500)
         set_log_level(LogLevel.NONE)
+        OPTIONS["docker"] = True
 
     @classmethod
     def tearDownClass(cls) -> None:
-        delete_directory("tmp")
+        # delete_directory("tmp")
+        pass
 
     def test_emotet1(self):
-        self.assertEqual(test("emotet1"), 0)
+        self.assertEqual(0, test("malwares", "emotet1"))
 
     def test_malware1(self):
-        self.assertEqual(test("malware1"), 0)
+        self.assertEqual(0, test("malwares", "malware1"))
 
     def test_malware2(self):
-        self.assertEqual(test("malware2"), 0)
+        self.assertEqual(0, test("malwares", "malware2"))
+
+    def test_malware3(self):
+        self.assertEqual(0, test("malwares", "malware3"))
+
+    def test_malware5(self):
+        self.assertEqual(0, test("malwares", "malware5"))
